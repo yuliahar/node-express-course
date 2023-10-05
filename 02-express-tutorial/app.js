7b03b60d362b43857ea08a61db8d1ed4d29902e0
Yuliya Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2023-10-05 09:15:29 -0400

    feat: implement API endpoints: add routes for fetching all products and individual products by ID

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,29 @@
+const { products } = require("./data");
 const express = require('express');
 
 const app = express();
 
+// setup static and middleware
 app.use(express.static("./public"));
 
+// routes
+app.get('/api/v1/test', (req, res) => {
+  res.json({ message: "It worked!" });
+});
+
+app.get('/api/v1/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/v1/products/:productID', (req, res) => {
+  const idToFind = parseInt(req.params.productID);
+  const product = products.find((p) => p.id === idToFind);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: "That product was not found." });
+  }
+});
 
 app.all('*', (req, res) => {
   res.status(404).send('<h1>Page not found</h1>');
