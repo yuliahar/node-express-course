03a8044e802c1c159eee5637dff5be3363e10947
Yuliya Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2023-10-05 08:49:08 -0400

    feat: initialize Express application: add public/index.html and set up basic express app in app.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,14 @@
-console.log('Express Tutorial')
+const express = require('express');
+
+const app = express();
+
+app.use(express.static("./public"));
+
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>Page not found</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000...');
+});
