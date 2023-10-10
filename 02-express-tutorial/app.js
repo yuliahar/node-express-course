bf2d1225476dd9b95c3bcca7f7b180f537b5bdde
Yuliya Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2023-10-10 11:26:40 -0400

    feat: add fetch button to index.html for API products data display
    - Create a button in index.html to fetch product data, implement js code to handle button click

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -11,10 +11,12 @@ app.get('/api/v1/test', (req, res) => {
   res.json({ message: "It worked!" });
 });
 
+// http://localhost:3000/api/v1/products
 app.get('/api/v1/products', (req, res) => {
   res.json(products);
 });
 
+// http://localhost:3000/api/v1/products/1
 app.get('/api/v1/products/:productID', (req, res) => {
   const idToFind = parseInt(req.params.productID);
   const product = products.find((p) => p.id === idToFind);
@@ -26,19 +28,56 @@ app.get('/api/v1/products/:productID', (req, res) => {
 });
 
 app.get('/api/v1/query', (req, res) => {
-  const { search, limit } = req.query;
+  const { search, limit, minPrice, maxPrice } = req.query;
   let sortedProducts = [...products];
 
-  if (search) {
+  // Check if search query contains special regex characters
+  const hasSpecialChars = /[\\^$*+?.()|[\]{}]/.test(search);
+
+  if (hasSpecialChars) {
+    // http://localhost:3000/api/v1/query?search=^al&limit=5
+    // http://localhost:3000/api/v1/query?search=a$&limit=5
+    try {
+      const regex = new RegExp(search, 'i'); // 'i' for case-insensitive
+      sortedProducts = sortedProducts.filter((product) => regex.test(product.name));
+    } catch (e) {
+      // http://localhost:3000/api/v1/query?search=*&limit=5
+      return res.json({ success: false, message: "Invalid regular expression." });
+    }
+  } else {
+    // http://localhost:3000/api/v1/query?search=al&limit=5
     sortedProducts = sortedProducts.filter((product) => product.name.startsWith(search));
   }
 
+  if (minPrice) {
+    if (!isNaN(minPrice)) {
+      // http://localhost:3000/api/v1/query?search=al&limit=5&minPrice=10
+      sortedProducts = sortedProducts.filter((product) => product.price >= Number(minPrice));
+    } else {
+      // http://localhost:3000/api/v1/query?search=al&limit=5&minPrice=a
+      return res.json({ success: false, message: "Please enter a valid minimum price." });
+    }
+  }
+
+  if (maxPrice) {
+    if (!isNaN(maxPrice)) {
+      // http://localhost:3000/api/v1/query?search=al&limit=5&minPrice=10&maxPrice=20
+      sortedProducts = sortedProducts.filter((product) => product.price <= Number(maxPrice));
+    } else {
+      // http://localhost:3000/api/v1/query?search=al&limit=5&minPrice=10&maxPrice=d
+      return res.json({ success: false, message: "Please enter a valid maximum price." });
+    }
+  }
+
+  // http://localhost:3000/api/v1/query?search=al&limit=2
   if (limit && Number(limit) > 0) {
     sortedProducts = sortedProducts.slice(0, Number(limit));
   } else {
+    // http://localhost:3000/api/v1/query?search=al&limit=d
     return res.json({ success: true, message: "Please enter a limit number greater than 0." });
   }
 
+  // http://localhost:3000/api/v1/query?search=3&limit=2
   if (sortedProducts.length < 1) {
     return res.json({ success: true, message: "No products matched your search." });
   }
