f9527d97b6a09261525d92684f0d29c1b62b0747
Yuliya Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2023-10-31 20:50:47 -0400

    refactor: add middleware to handle error, add middleware to wrap trycatch block

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,6 +3,8 @@ const app = new express();
 const connectDB = require('./db/connect');
 const tasksRouter = require('./routes/tasks');
 const dotenv = require('dotenv');
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 dotenv.config();
 
@@ -12,6 +14,9 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/tasks', tasksRouter);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,121 +1,91 @@
 const Task = require("../models/Task");
+const mongoose = require('mongoose');
+const asyncWrapper = require("../middleware/async");
+const { createCustomError } = require("../errors/custom-error");
 
-const getAllTasks = async (req, res) => { // get all tasks
-  try {
-    const tasks = await Task.find({});
+const getAllTasks = asyncWrapper(async (req, res) => { // get all tasks
+  const tasks = await Task.find({});
+  res.status(200).json({
+    success: true,
+    tasks,
+    amount: tasks.length
+  });
+});
 
-    res.send({
-      success: true,
-      tasks,
-      amount: tasks.length
-    });
-  } catch (err) {
-    res.status(500).json({
+const getTask = asyncWrapper(async (req, res, next) => { // get a single task
+  const { id: taskID } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(taskID)) {
+    return next(createCustomError(`Invalid id : ${taskID}`, 400));
+  }
+
+  const task = await Task.findOne({ _id: taskID });
+  if (!task) {
+    // test with: http://localhost:5000/api/v1/tasks/65370b491c66b746be09fd99
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
+  }
+  res.status(200).json({ task });
+});
+
+const createTask = asyncWrapper(async (req, res) => { // create a new task
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({
       success: false,
-      message: err.message
+      message: "Name is required"
     });
   }
-}
 
-const getTask = async (req, res) => { // get a single task
-  try {
-    const { id: taskID } = req.params
-    const task = await Task.findOne({ _id: taskID })
-    if (!task) {
-      // test with: http://localhost:5000/api/v1/tasks/65370b491c66b746be09fd99
-      return res.status(404).json({
-        success: false,
-        message: `No task with id: ${taskID}`
-      });
-    }
-    res.status(200).json({ task })
-  } catch (err) {
-    // test with: http://localhost:5000/api/v1/tasks/1
-    res.status(500).json({
-      success: false,
-      message: err.message
-    });
+  const task = await Task.create(req.body);
+  res.status(201).send({
+    success: true,
+    message: "Task created successfully",
+    task
+  });
+});
+
+const updateTask = asyncWrapper(async (req, res, next) => { // update task
+  const { id: taskID } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(taskID)) {
+    return next(createCustomError(`Invalid id : ${taskID}`, 400));
   }
-}
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-const createTask = async (req, res) => { // create a new task
-  try {
-    const { name } = req.body;
-
-    if (!name) {
-      return res.status(400).json({
-        success: false,
-        message: "Name is required"
-      });
-    }
-
-    const task = await Task.create(req.body);
-    res.status(201).send({
-      success: true,
-      message: "Task created successfully",
-      task
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: err.message
-    });
+  if (!task) {
+    // test with: http://localhost:5000/api/v1/tasks/65370b491c66b746be09fd99
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
-}
 
-const updateTask = async (req, res) => { // update task
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      new: true,
-      runValidators: true
-    });
+  res.status(200).json({
+    success: true,
+    message: "Task updated successfully",
+    task
+  });
+});
 
-    if (!task) {
-      // test with: http://localhost:5000/api/v1/tasks/65370b491c66b746be09fd99
-      return res.status(404).json({
-        success: false,
-        message: `No task with id: ${taskID}`
-      });
-    }
+const deleteTask = asyncWrapper(async (req, res, next) => { // delete task
+  const { id: taskID } = req.params
 
-    res.status(200).json({
-      success: true,
-      message: "Task updated successfully",
-      task
-    })
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: err.message
-    });
+  if (!mongoose.Types.ObjectId.isValid(taskID)) {
+    return next(createCustomError(`Invalid id : ${taskID}`, 400));
   }
-}
 
-const deleteTask = async (req, res) => { // delete task
-  try {
-    const { id: taskID } = req.params
-    const task = await Task.findOne({ _id: taskID });
+  const task = await Task.findOne({ _id: taskID });
 
-    if (!task) {
-      return res.status(404).json({
-        success: false,
-        message: `No task with id: ${taskID}`
-      });
-    }
-    await task.remove();
-    res.status(200).json({
-      success: true,
-      message: "Task deleted successfully"
-    });
-  } catch (err) {
-    // test with: http://localhost:5000/api/v1/tasks/1
-    res.status(500).json({
-      success: false,
-      message: err.message
-    });
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
-}
+  await task.remove();
+  res.status(200).json({
+    success: true,
+    message: "Task deleted successfully"
+  });
+});
 
 module.exports = {
   getAllTasks,
